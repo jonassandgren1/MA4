c4589174234f8befa1577269e1be909bce7ed8f0
jonassandgren1 <jonassandgren1@gmail.com>
2023-10-11 10:26:44 +0200

    First try to calc time for py code.

--- MA4_2.py
+++ MA4_2.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
 from person import Person
+from time import perf_counter as pc
+import matplotlib.pyplot as plt
+
 
 def main():
 	f = Person(5)
 	print(f.get())
+
+
+	start = pc()
 	f.set(7)
 	print(f.fib())
+	end = pc()
+	print(f'fib in python took: {end-start} sec to run')
 
 if __name__ == '__main__':
 	main()
